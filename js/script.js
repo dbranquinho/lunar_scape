068461595c42d18226ae239c4222cd589da9713f
dbranquinho <delermando@gmail.com>
2025-03-17 14:36:59 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-button').addEventListener('click', function() {
+    // Aqui você pode redirecionar para o jogo principal ou iniciar a lógica do jogo.
+    alert('Jogo será iniciado!'); // Substitua isso pela lógica real do jogo
+});
